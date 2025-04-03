599004e950d368cffcb544c2358c5e5a1e3362fc
HasnaHasan194 <hasnahasnahasan194@gmail.com>
2025-04-03 20:53:26 +0530

    link updated

--- src/components/UserComponent/Account/PersonalInformation.jsx
+++ src/components/UserComponent/Account/PersonalInformation.jsx
@@ -306,7 +306,7 @@ const PersonalInformation = () => {
 
   const shareReferralLink = () => {
     const referralCode = userDetails.referralCode;
-    const referralLink = `https://readhaven-frontend.vercel.app//signup?ref=${referralCode}`;
+    const referralLink = `https://readhaven-frontend.vercel.app/signup?ref=${referralCode}`;
 
     if (navigator.share) {
       navigator
